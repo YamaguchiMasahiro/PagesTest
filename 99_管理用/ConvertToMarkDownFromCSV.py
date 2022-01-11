4f41d1cec0da753d1c687ac128df95fb82ff0cca
YamaguchiMasahiro <yamaguchi.masahiro@isid.co.jp>
2022-01-11 15:20:01 +0900

    更新

--- 99_管理用/ConvertToMarkDownFromCSV.py
+++ 99_管理用/ConvertToMarkDownFromCSV.py
@@ -21,14 +21,14 @@ def CreateStandardTable():
             if(row["解説／対策"] != ""):
                 file_path = row["基準"] + "/" + row["ID"] + ".md"
                 # 詳細ファイル作成
-                CreateDetailMarkdown(row,file_path)
+                CreateDetailMarkdown(row, "../" + file_path)
                 # テーブルの値を更新
                 row["解説／対策"] = LINK_FORMAT.replace("%s1",row["ID"]).replace("%s2", file_path)
             column_list = [row[key] for key in row.keys()]
             record = "|" + "|".join(column_list) + "|"
             record = record.replace("\n","</br>")
             records = records + record + "\n"
-    with open("01_設定基準一覧.md", mode='w', encoding='utf-8') as fw:
+    with open("../10_設定基準一覧.md", mode='w', encoding='utf-8') as fw:
         fw.write(records)
 
 def CreateDetailMarkdown(raw,file_path):
@@ -38,10 +38,8 @@ def CreateDetailMarkdown(raw,file_path):
         records = records.replace("【①：リスク】","## リスク\n")
         records = records.replace("【②：対策】","\n## 対策\n")
         records = records.replace("【③：②を行わない場合のリスク軽減策】","\n## 対策を行わない場合のリスク軽減策\n")
-        #records = records.replace("（実施時のTIP）","\n### 実施時のTIP\n") 
         records = re.sub(r'\n（(.*)）\n', "\n\n### \\1\n\n", records)
         # URL変換
-        #records = re.sub(r'\n(https.*)\n', '\n  - <\\1>\n', records)
         records = re.sub(r'\n・?(https.*)\n', '\n- <\\1>\n', records)
         records = re.sub(r'\n・', "\n- ", records)
         records = re.sub(r'\n　・?', "\n  - ", records)
